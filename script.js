c0c0657823c01f304253adac4c319e82d3a5e901
SterlingOrth <71182922+SterlingOrth@users.noreply.github.com>
2020-10-14 13:08:49 -0600

    create html css and js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Variables to count correct and incorrect answers
+// Create a welcome page with a clickable 'button' that will advance you to next question
+// This button will also start an internal sticky timer
+// Clear previous and start new object on page with sticky timer
+// First question with 4 clickable 'buttons'
+// If for correct statement Else for false answers and push results to variables 'correct' and 'incorrect'
+// After question 10, return 'correct' and 'incorrect' results, followed by "You Won" or "You Lost" with 
+// 
