42442bb4bedd4268785cf1b4c3c2e804a50a6307
SterlingOrth <71182922+SterlingOrth@users.noreply.github.com>
2020-10-15 11:02:49 -0600

    created buttons in html

--- script.js
+++ script.js
@@ -1,8 +1,133 @@
-// Variables to count correct and incorrect answers
-// Create a welcome page with a clickable 'button' that will advance you to next question
-// This button will also start an internal sticky timer
-// Clear previous and start new object on page with sticky timer
-// First question with 4 clickable 'buttons'
-// If for correct statement Else for false answers and push results to variables 'correct' and 'incorrect'
-// After question 10, return 'correct' and 'incorrect' results, followed by "You Won" or "You Lost" with 
-// 
+// Variables to count correct answers 
+// var timer = 
+
+// document.getElementById("#button")
+
+// setInterval(function(){ alert("Hello"); }, 3000);
+
+
+alert("Hello")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Sample
+// var questionList = [
+//     {
+//         question: "First question here?\n(a) CHOICE ONE\n\
+//                     (b) CHOICE TWO/n(c) CHOICE THREE",
+//         answer: "ABorC"
+//     },
+//     {
+//         question: "First question here?\n(a) CHOICE ONE\n\
+//                     (b) CHOICE TWO/n(c) CHOICE THREE",
+//         answer: "ABorC"
+//     },
+//     {
+//         question: "First question here?\n(a) CHOICE ONE\n\
+//                     (b) CHOICE TWO/n(c) CHOICE THREE",
+//         answer: "ABorC"
+//     },
+//     {
+//         question: "First question here?\n(a) CHOICE ONE\n\
+//                     (b) CHOICE TWO/n(c) CHOICE THREE",
+//         answer: "ABorC"
+//     },
+//     {
+//         question: "First question here?\n(a) CHOICE ONE\n\
+//                     (b) CHOICE TWO/n(c) CHOICE THREE",
+//         answer: "ABorC"
+//     },
+//     {
+//         question: "First question here?\n(a) CHOICE ONE\n\
+//                     (b) CHOICE TWO/n(c) CHOICE THREE",
+//         answer: "ABorC"
+//     },
+//     {
+//         question: "First question here?\n(a) CHOICE ONE\n\
+//                     (b) CHOICE TWO/n(c) CHOICE THREE",
+//         answer: "ABorC"
+//     },
+//     {
+//         question: "First question here?\n(a) CHOICE ONE\n\
+//                     (b) CHOICE TWO/n(c) CHOICE THREE",
+//         answer: "ABorC"
+//     },
+//     {
+//         question: "First question here?\n(a) CHOICE ONE\n\
+//                     (b) CHOICE TWO/n(c) CHOICE THREE",
+//         answer: "ABorC"
+//     },
+//     {
+//         question: "First question here?\n(a) CHOICE ONE\n\
+//                     (b) CHOICE TWO/n(c) CHOICE THREE",
+//         answer: "ABorC"
+//     },
+//    ]
+// var correctAnswer = 0;
+
+// // For loop to register correct guesses
+// for(var i=0: i < questionList.length; i++)
+//     var userChoice = window.question(questionList[i].question);
+//     if(userChoice === questionList[i].answer){
+//         correctAnswer++;
+//         alert("You got it Right!")
+//     } else {
+//         alert("That is why you fail..")
+//     }
+
+//     alert("Final Score: " + correctAnswer + "out of " + questionList.length);
+
+// // Display sticky timer that begins "on click" of start 'button'
+
+// // var clear; function stopWatch( ) { // javascript statement here clear = setTimeout( "stopWatch( )", 1000 ); } 
+// // Or function stopWatch( ) { // javascript statement here clear = setTimeout( function ( ) { // javascript statement here }, 1000 ); } 
+// // Or var stopWatch = function ( ) { // javascript statement here clear = setTimeout( "stopWatch( )", 1000 ); } 
+
+// // Create a welcome page with a clickable 'button' that will advance you to next question
+// // This button will also start an internal sticky timer
+// // Clear previous and start new object on page with sticky timer
+// // First question with 4 clickable 'buttons'
+// // If for correct statement Else for false answers and push results to variables 'correct' and 'incorrect'
+// // After question 10, return 'correct' and 'incorrect' results,
+// // stop timer, followed by "You Won" or "You Lost" with >= 6 in correctCounter
+
+
+
+
+
+// // clearTimeout( return ID of setTimeout() );
