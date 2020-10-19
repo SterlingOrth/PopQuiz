8aefac659f01dea624d7ccb44f964c07366411a5
SterlingOrth <71182922+SterlingOrth@users.noreply.github.com>
2020-10-18 21:57:16 -0600

    Timer stops on last question

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # PopQuiz
 
+Welcome to the Hopelessly-Pointless, PopQuiz! This quiz was designed to be completely random and pointless, so play at your own risk! All the answers were designed to be completely random so good luck!
+
+Quiz consists of 10 random questions, with a timer counting down from 120 seconds. When the timer reaches zero, user should be taken to 'high-score' page. Alternatively, if user completes all 10 questions within alloted amount of time, they will also be taken to 'high-score' page.
+
+References:
+https://github.com/SterlingOrth/PopQuiz.git
+https://sterlingorth.github.io/PopQuiz/
+
+Sources:
+https://www.youtube.com/watch?v=riDzcEQbX6k&amp;t=2s
+
+Screenshot:
+<img src="Screenshot.png" alt="screenshot">
+
 <!--
-* Attach timer to start button and stop when last question is asked\
+* Timer stop when last question submitted
 ** Display score and allow place to enter name
 *** Create list of questions
 **** Subtract Seconds on wrong answer
--- script.js
+++ script.js
@@ -4,10 +4,15 @@ var nextButton = document.getElementById('next-btn')
 var questionContainerElement = document.getElementById
 ('question-container')
 var timerEl = document.getElementById("timer");
+var scoreCheck = document.getElementById("score-check")
+
+var timeLeft = 120;
+
 
 var questionElement = document.getElementById('question')
 var answerButtonsElement = document.getElementById("answer-buttons")
 
+var timeInterval
 // random question placement variable stored in global memory
 let shuffleQuestions, currentQuestionIndex
 
@@ -21,9 +26,7 @@ nextButton.addEventListener('click', () => {
 
 // timer
 function setTime() {
-    var timeLeft = 120;
-  
-    var timeInterval = setInterval(function() {
+    timeInterval = setInterval(function() {
       timerEl.textContent = timeLeft + " seconds remaining";
       timeLeft--;
   
@@ -71,7 +74,6 @@ function resetState() {
     nextButton.classList.add('hide');
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-
     }
 }
 
@@ -84,11 +86,16 @@ function userChoice (e) {
     })
     if (shuffleQuestions.length > currentQuestionIndex + 1) {
         nextButton.classList.remove('hide')
-    } else {
-    startButton.innerText = 'Restart',
-    startButton.classList.remove("hide");
+    } 
+    else {
+        clearInterval(timeInterval);
     }
-}
+    if (selectedButton.dataset !== correct) {
+        timeLeft -= 5;
+        scoreCheck.textContent = "Wrong! Lost 5 seconds!"
+        // HOW TO CLEAR TEXT!?
+            }
+    }
 function setStatusClass(element, correct) {
     clearStatusClass(element)
     if (correct) {
@@ -109,137 +116,95 @@ var questions = [
     {
         question: "Will you Choose A, B, C or D... I wonder??",
         answers: [
-            {text: "Correct answer", correct: true},
-            {text: "False #1", correct: false},
-            {text: "False #2", correct: false},
-            {text: "False #3", correct: false},
+            {text: "B", correct: true},
+            {text: "A", correct: false},
+            {text: "C", correct: false},
+            {text: "D", correct: false},
         ]
     },
     {
         question: "Will you Choose A, B, C or D... I wonder??",
         answers: [
-            {text: "Correct answer", correct: true},
-            {text: "False #1", correct: false},
-            {text: "False #2", correct: false},
-            {text: "False #3", correct: false},
+            {text: "A", correct: true},
+            {text: "B", correct: false},
+            {text: "C", correct: false},
+            {text: "D", correct: false},
         ]
     },
     {
         question: "Will you Choose A, B, C or D... I wonder??",
         answers: [
-            {text: "Correct answer", correct: true},
-            {text: "False #1", correct: false},
-            {text: "False #2", correct: false},
-            {text: "False #3", correct: false},
+            {text: "D", correct: true},
+            {text: "C", correct: false},
+            {text: "B", correct: false},
+            {text: "A", correct: false},
         ]
     },
     {
         question: "Will you Choose A, B, C or D... I wonder??",
         answers: [
-            {text: "Correct answer", correct: true},
-            {text: "False #1", correct: false},
-            {text: "False #2", correct: false},
-            {text: "False #3", correct: false},
+            {text: "A", correct: true},
+            {text: "C", correct: false},
+            {text: "D", correct: false},
+            {text: "B", correct: false},
         ]
     },
     {
         question: "Will you Choose A, B, C or D... I wonder??",
         answers: [
-            {text: "Correct answer", correct: true},
-            {text: "False #1", correct: false},
-            {text: "False #2", correct: false},
-            {text: "False #3", correct: false},
+            {text: "C", correct: true},
+            {text: "A", correct: false},
+            {text: "B", correct: false},
+            {text: "D", correct: false},
         ]
     },
     {
         question: "Will you Choose A, B, C or D... I wonder??",
         answers: [
-            {text: "Correct answer", correct: true},
-            {text: "False #1", correct: false},
-            {text: "False #2", correct: false},
-            {text: "False #3", correct: false},
+            {text: "B", correct: true},
+            {text: "A", correct: false},
+            {text: "D", correct: false},
+            {text: "C", correct: false},
         ]
     },
     {
         question: "Will you Choose A, B, C or D... I wonder??",
         answers: [
-            {text: "Correct answer", correct: true},
-            {text: "False #1", correct: false},
-            {text: "False #2", correct: false},
-            {text: "False #3", correct: false},
+            {text: "B", correct: true},
+            {text: "A", correct: false},
+            {text: "D", correct: false},
+            {text: "C", correct: false},
         ]
     },
     {
         question: "Will you Choose A, B, C or D... I wonder??",
         answers: [
-            {text: "Correct answer", correct: true},
-            {text: "False #1", correct: false},
-            {text: "False #2", correct: false},
-            {text: "False #3", correct: false},
+            {text: "A", correct: true},
+            {text: "B", correct: false},
+            {text: "C", correct: false},
+            {text: "D", correct: false},
         ]
     },
     {
         question: "Will you Choose A, B, C or D... I wonder??",
         answers: [
-            {text: "Correct answer", correct: true},
-            {text: "False #1", correct: false},
-            {text: "False #2", correct: false},
-            {text: "False #3", correct: false},
+            {text: "D", correct: true},
+            {text: "B", correct: false},
+            {text: "A", correct: false},
+            {text: "C", correct: false},
         ]
     },
     {
         question: "Will you Choose A, B, C or D... I wonder??",
         answers: [
-            {text: "Correct answer", correct: true},
-            {text: "False #1", correct: false},
-            {text: "False #2", correct: false},
-            {text: "False #3", correct: false},
+            {text: "C", correct: true},
+            {text: "B", correct: false},
+            {text: "D", correct: false},
+            {text: "A", correct: false},
         ]
     }
-]
+   ]
 
-
-
-
-
-
-
-
-// Variables to count correct answers 
-// var timer = 
-
-// document.getElementById("#button")
-
-// setInterval(function(){ alert("Hello"); }, 3000);
-
-
-
-
-// // Sample
-// var correctAnswer = 0;
-
-// // For loop to register correct guesses
-// for(var i=0: i < questionList.length; i++)
-//     var userChoice = window.question(questionList[i].question);
-//     if(userChoice === questionList[i].answer){
-//         correctAnswer++;
-//         alert("You got it Right!")
-//     } else {
-//         alert("That is why you fail..")
-//     }
-
-//     alert("Final Score: " + correctAnswer + "out of " + questionList.length);
-
-// // Display sticky timer that begins "on click" of start 'button'
-
-// // var clear; function stopWatch( ) { // javascript statement here clear = setTimeout( "stopWatch( )", 1000 ); } 
-// // Or function stopWatch( ) { // javascript statement here clear = setTimeout( function ( ) { // javascript statement here }, 1000 ); } 
-// // Or var stopWatch = function ( ) { // javascript statement here clear = setTimeout( "stopWatch( )", 1000 ); } 
-
-// // Create a welcome page with a clickable 'button' that will advance you to next question
-// // This button will also start an internal sticky timer
-// // Clear previous and start new object on page with sticky timer
-// // First question with 4 clickable 'buttons'
-// // If for correct statement Else for false answers and push results to variables 'correct' and 'incorrect'
-// // After question 10, return 'correct' and 'incorrect' results,
-//  Timer stops on last question click
+   if (question.answers === false) {
+        var reduceTime;
+}
